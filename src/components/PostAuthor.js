b5cdc277a6888050694166d921ef0f2acb2cf4b3
asimonv <aasimon@uc.cl>
2022-03-15 02:25:21 -0300

    hotfix dynamic author image

--- src/components/PostAuthor.js
+++ src/components/PostAuthor.js
@@ -1,10 +1,10 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
-const PostAuthor = ({ author: { name } }) => {
+const PostAuthor = ({ author: { name, profileImage } }) => {
   return (
     <figure className="flex items-center space-x-2">
-      <StaticImage src="../../static/me-c.jpg" width="40" placeholder="blurred" />
+      <StaticImage src={`../../static/${profileImage}`} width="40" placeholder="blurred" />
       <figcaption className="font-medium">{name}</figcaption>
     </figure>
   );
